3a9611d673eac92e2a74d11392a7b198ec86b8fa
Darshana Sirisena <117305837+DarshanaSi@users.noreply.github.com>
2023-05-17 23:44:12 +0200

    Update server.js

--- week1/homework/src/server.js
+++ week1/homework/src/server.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-const http = require('http');
+const http = require("http");
 
 /* `createServer` MUST return an instance of `http.Server` otherwise the tests
  * will fail.
@@ -9,12 +9,32 @@ function createServer(port) {
   let state = 10;
 
   const server = http.createServer((request, response) => {
-    // TODO: Write your homework code here
+    if (request.method === "GET") {
+      if (request.url === "/state" || request.url === "/") {
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ state }));
+      } else if (request.url === "/add") {
+        state++;
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ state }));
+      } else if (request.url === "/subtract") {
+        state--;
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ state }));
+      } else if (request.url === "/reset") {
+        state = 10;
+        response.writeHead(200, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ state }));
+      } else {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify({ error: "Not found" }));
+      }
+    }
   });
 
   return server;
 }
 
 module.exports = {
-  createServer
+  createServer,
 };
